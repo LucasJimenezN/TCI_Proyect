d17f1ce9f1df5c702f10bbc7870623805ed6caae
Lucas Jiménez Núñez <ljimenez2nunez@gmail.com>
2023-10-25 09:14:44 +0200

    MSE, PSNR, PAE, done

--- TCIProject/codingTools/BitPacking.py
+++ TCIProject/codingTools/BitPacking.py
@@ -5,9 +5,9 @@ from .utils import *
 from .binary_file_output import *
 from .binary_file_input import *
 
-#object to manipulate a output binary file
+#This bitpacking only works for unsigned images
 class BitPacking:
-    
+
     #constructor. 
     def __init__(self, file_name, image_data):
         self.file_name = file_name
@@ -20,10 +20,36 @@ class BitPacking:
     def coding(self):
         BFO = binary_file_output(self.file_name)
         BFO.open_binay_file_output()
-        #TO BE IMPLEMENTED
+        
+        min = self.image_data.min()
+        max = self.image_data.max()
+        dynamic_range = max-min
+        needed_bits_to_write = needed_bits(dynamic_range) if dynamic_range != 0 else 1
+        
+        BFO.write_value(max,16)
+        BFO.write_value(min,16)
+        
+        for z in range(self.image_data.shape[0]):
+            for y in range(self.image_data.shape[1]):
+                for x in range(self.image_data.shape[2]):
+                    BFO.write_value(self.image_data[z][y][x],needed_bits_to_write)
+        
+        self.size_of_coded_file = BFO.size_of_file()
+        
+        BFO.close_binary_file_output()
         
     def decoding(self):
         BFI = binary_file_input(self.file_name)
         BFI.open_binay_file_input()
-        #TO BE IMPLEMENTED
+        max = int(BFI.read_value(16))
+        min = int(BFI.read_value(16))
+        print("min ",min)
+        print("max ",max)
+        needed_bits_to_read = needed_bits(max-min)
+        print("needed_bits_to_read ",needed_bits_to_read)
+        for z in range(self.image_data.shape[0]):
+            for y in range(self.image_data.shape[1]):
+                for x in range(self.image_data.shape[2]):
+                    self.image_data[z][y][x] = BFI.read_value(needed_bits_to_read)
+
         
